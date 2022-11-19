f737384375375e49181ea05b094bf9a4fa06f81c
raulito <raulito@MacBook-Air-de-Jose.local>
2022-11-19 12:01:09 -0600

    Primera parte

--- api/src/config.js
+++ api/src/config.js
@@ -3,8 +3,16 @@ import { config } from "dotenv";
 config();
 
 export default {
-    host: "127.0.0.1",
-    database: "proyectoso",
-    user: "root",
-    password: ""
+    database: process.env.database || "proyectoso",
+    host: process.env.host || "127.0.0.1",
+    user: process.env.username || "root",
+    password: process.env.password || ""
 };
+
+
+/*
+database=proyectoso
+username=xo37xwod03gj4yxlpp9b
+host=us-east.connect.psdb.cloud
+password=pscale_pw_jQhbvWgm1LtkMQ6r0EAE8tc2ZQ0cam3PXTfnzuuHRZV
+*/
